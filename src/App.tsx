3a644f153d4648ca4145e070565ebed59059dff1
Daniel Mizerski <duelsik@gmail.com>
2022-02-16 18:08:28 +0100

    (feature) Add bridge.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-// import React from "react";
-// import styled from "styled-components";
+import React, {useCallback} from "react";
+import qs from "query-string";
+import styled from "styled-components";
 import {QueryClient, QueryClientProvider, useQuery} from "react-query";
+import {BeatSaber} from "@duelsik/twitch-overlay-toolkit";
+
+type SearchParams = {
+  secret?: string; // don't do that at home kids
+  bridgeMode?: string;
+};
+const searchParams = qs.parse(window.location.search) as unknown as SearchParams;
 
 const queryClient = new QueryClient();
 
+type PostRequestBody = {
+  playerName: string;
+  secret: string;
+};
+
 type HistoryItem = {
   mapName: string;
+  timestamp: number;
   coverUrl: string;
   mapHash: string;
   bsrCode: string;
 };
 
+const HistoryItemWrapper = styled.div`
+  margin-bottom: 2rem;
+`;
+const CoverImg = styled.img`
+  float: left;
+  width: 5rem;
+  height: 5rem;
+  margin-right: 2rem;
+  border: 0px solid black;
+  border-radius: 1rem;
+`;
 const renderHistoryItem = ({mapName, coverUrl, mapHash, bsrCode}: HistoryItem) => {
   return (
-    <p>
-      <img style={{float: "left"}} src={coverUrl} />
+    <HistoryItemWrapper>
+      <CoverImg src={coverUrl} />
       <span>
         <div>{mapName}</div>
         <div><code>{mapHash}</code></div>
         <div><code>{bsrCode}</code></div>
       </span>
-    </p>
+    </HistoryItemWrapper>
   );
 }
 
@@ -38,9 +63,102 @@ const Listing = () => {
   );
 }
 
+const ListingInput = () => {
+  const isEnabled = !!searchParams.secret;
+  const pseudoUserAuth = {
+    playerName: "test",
+    secret: searchParams.secret as string,
+  };
+  const testData: HistoryItem = {
+    bsrCode: "test",
+    mapHash: "test",
+    mapName: "test",
+    coverUrl: "https://wallup.net/wp-content/uploads/2016/03/12/305164-nature-cat.jpg",
+    timestamp: Date.now()
+  };
+  const pushItem = React.useCallback(() => {
+    fetch("/api/history/push", {
+      method: "POST",
+      body: JSON.stringify({...testData, ...pseudoUserAuth}),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+  }, []);
+  const clearAll = React.useCallback(() => {
+    fetch("/api/history/clearAll", {
+      method: "POST",
+      body: JSON.stringify({...pseudoUserAuth}),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+  }, []);
+
+
+  if (!isEnabled) return null;
+
+  return (
+    <div>
+      <button onClick={pushItem}>Push data</button>
+      <button onClick={clearAll}>Clear all</button>
+    </div>
+  );
+}
+
+const BridgeComponent = () => {
+  const pseudoUserAuth = {
+    playerName: "test",
+    secret: searchParams.secret as string,
+  };
+  const onSongStart: BeatSaber.HTTPStatus.SongStartHandler = useCallback(async (event) => {
+    const {beatmap} = event.status;
+
+    if (!beatmap) return;
+
+    const {songHash, songName, songAuthorName, levelAuthorName} = beatmap;
+    const beatsaverDataResponse = await fetch(`https://api.beatsaver.com/hash/${songHash}`);
+    const beatsaverData = await beatsaverDataResponse.json() ?? {id: "none"};
+    const id = beatsaverData.id as string;
+    const coverUrl = beatsaverData?.versions[0]?.coverUrl;
+
+    const mapData: HistoryItem = {
+      bsrCode: id,
+      mapHash: songHash,
+      mapName: `${songName} (${songAuthorName}) mapped by ${levelAuthorName}`,
+      coverUrl: "https://wallup.net/wp-content/uploads/2016/03/12/305164-nature-cat.jpg",
+      timestamp: Date.now()
+    };
+    fetch("/api/history/push", {
+      method: "POST",
+      body: JSON.stringify({...mapData, ...pseudoUserAuth}),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+  }, []);
+
+  BeatSaber.useHTTPStatusWebsocket({
+    address: "ws://localhost:6557",
+    debug: false,
+    errorHandler: console.error
+  }, [
+    {
+     songStart: onSongStart
+    }
+  ]);
+  
+  return null;
+};
+
 const App = () => {
+  if (!!searchParams.bridgeMode) {
+    return <BridgeComponent />;
+  }
+
   return (
     <QueryClientProvider client={queryClient}>
+      <ListingInput />
       <Listing />
     </QueryClientProvider>
   );
