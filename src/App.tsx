bf9cdd07b327a3c693d4b2b9fb0e1c5e6700ec29
Daniel Mizerski <duelsik@gmail.com>
2022-02-16 16:56:49 +0100

    (feature) Test api listing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,49 @@
-import React from "react";
-import styled from "styled-components";
+// import React from "react";
+// import styled from "styled-components";
+import {QueryClient, QueryClientProvider, useQuery} from "react-query";
 
-const HiStyled = styled.div`
-  color: red;
-`;
-const App = () => {
-  return <HiStyled>Hi</HiStyled>;
+const queryClient = new QueryClient();
+
+type HistoryItem = {
+  mapName: string;
+  coverUrl: string;
+  mapHash: string;
+  bsrCode: string;
 };
+
+const renderHistoryItem = ({mapName, coverUrl, mapHash, bsrCode}: HistoryItem) => {
+  return (
+    <p>
+      <img style={{float: "left"}} src={coverUrl} />
+      <span>
+        <div>{mapName}</div>
+        <div><code>{mapHash}</code></div>
+        <div><code>{bsrCode}</code></div>
+      </span>
+    </p>
+  );
+}
+
+const Listing = () => {
+  const {isLoading, error, data} = useQuery('playerData', () =>
+    fetch('/api/history/test/list').then(res =>
+      res.json() as Promise<HistoryItem[]>
+    )
+  );
+
+  return (
+    <div>
+      {data && data.map(renderHistoryItem)}
+    </div>
+  );
+}
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Listing />
+    </QueryClientProvider>
+  );
+};
+
 export default App;
