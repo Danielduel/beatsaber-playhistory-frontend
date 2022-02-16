5f7ab1f6c9a7aee4815e6af5fa6342ebbf509d73
Daniel Mizerski <duelsik@gmail.com>
2022-02-16 16:31:21 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const HiStyled = styled.div`
+  color: red;
+`;
+const App = () => {
+  return <HiStyled>Hi</HiStyled>;
+};
+export default App;
